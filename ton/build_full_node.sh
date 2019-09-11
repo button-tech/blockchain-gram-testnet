3f895e509fc537aa0701e06e28f699869a1dc490
prazd <alexokda@list.ru>
2019-09-11 21:41:07 +0300

    update build node script

--- ton/build_full_node.sh
+++ ton/build_full_node.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-export DEBCONF_FRONTEND=noninteractive
-apt-get -y purge console-setup
-apt-get update
-apt-get -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" install -yq libghc-zlib-dev gperf build-essential make wget libreadline-dev git ccache libmicrohttpd-dev libssl-dev cmake libgflags-dev --allow-downgrades --allow-remove-essential --allow-change-held-packages
-apt-get -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" dist-upgrade -yq
 cd /root
-git clone --recurse-submodules -j8 https://github.com/ton-blockchain/ton
+wget https://test.ton.org/ton-blockchain-full.tar.xz
+tar -xvf ton-blockchain-full.tar.xz
 mkdir /root/ton-build
 cd /root/ton-build
-cmake /root/ton
-cmake --build . --target validator-engine
+cmake /root/ton-node
+make
 mkdir /var/ton-work
 mkdir /var/ton-work/db
 mkdir /var/ton-work/etc
@@ -19,7 +15,6 @@ chmod 775 -R /var/ton-work
 cd /var/ton-work/etc
 wget https://test.ton.org/ton-global.config.json
 myip=$(wget -qO- http://ipecho.net/plain | xargs echo)
-/root/ton-build/validator-engine/validator-engine -C /var/ton-work/etc/ton-global.config.json --db /var/ton-work/db/ --ip ${myip}:1338 -l  /var/log/ton-node/log
 cat << EOF > /etc/systemd/system/ton-node.service
 [Unit]
 Description=TON Node
@@ -28,7 +23,7 @@ StartLimitIntervalSec=0
 [Service]
 Type=simple
 User=root
-ExecStart=/root/ton-build/validator-engine/validator-engine -C /var/ton-work/etc/ton-global.config.json --db /var/ton-work/db/ --ip ${myip}:1338 -l /var/log/ton-node/log
+ExecStart=/root/ton-build/validator-engine/validator-engine -C /var/ton-work/etc/ton-global.config.json --db /var/ton-work/db/ --ip ${myip}:3000 -l /var/log/ton-node/log
 [Install]
 WantedBy=multi-user.target
 EOF
